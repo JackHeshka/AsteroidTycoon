5d118cee1053d1612b024e56ccb9bf5307e6a64b
JackHeshka <jack.heshka@rbe.sk.ca>
2025-05-27 09:27:26 -0600

    Began planet surface lerp heightmapping

--- planet.py
+++ planet.py
@@ -1,3 +1,5 @@
+from random import *
+
 class planet:
     planetMap = []
     location = ''
@@ -6,4 +8,43 @@ class planet:
         pass
 
     def resourceScatter(self):
-        pass
+        #Generate base plane
+        surface = [
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0]
+        ]
+
+        #Random height sample points
+        for i in len(surface)/3:
+            for j in len(surface[i])/3:
+                surface[3*i][3*j] = random()
+
+        #Linear interpolation
+        #Vertical nodes
+        for i in surface:
+            segment = i/3
+            for j in len(surface[i])/3:
+                surface[i][3*j] = self.lerp(surface[3*int(segment)][3*j],
+                                            surface[3*(int(segment)+1)][3*j],
+                                            segment-int(segment))
+        #Horizontal nodes
+        for i in len(surface)/3:
+            for j in surface[i]:
+                surface[3*i][j] = self.lerp(surface[3*i][3*int(segment)],
+                                            surface[3*i][3*(int(segment)+1)],
+                                            segment-int(segment))
+        #Average over the surface
+
+
+    def lerp(self, a, b, t):
+        return (1-t)*a + t*b

cc8c52f4b4a5913df0ac193b2601c5f4048cc4c5
JackHeshka <jack.heshka@rbe.sk.ca>
2025-06-09 09:28:37 -0600

    Added comments and doc strings to planet.py and drill.py

--- drill.py
+++ drill.py
@@ -3,15 +3,23 @@ import time as t
 
 class drill:
     def __init__(self, oreAbundance, common, mid, rare):
-        self.curr: float = t.time() #Time to add productivity
+        self.curr: float = t.time()
         self.productivity = oreAbundance
         self.common = common
         self.mid = mid
         self.rare = rare
     
     def mine(self, player):
+        '''
+        This function will calculate how much ore was produced by the drill
+        based on time passed, and concentration at its location.
+        It then adds the resources to the players inventory and returns
+        a log of what was produced
+        '''
+        # Calculate time passed, in seconds, and multiply by productivity
         produced = int(t.time() - self.curr)*self.productivity
         self.curr = t.time()
+        # Add resources to player inventory
         player.raw_inventory[self.common] += round(0.7*produced)
         player.raw_inventory[self.mid] += round(0.25*produced)
         player.raw_inventory[self.rare] += round(0.05*produced)
--- planet.py
+++ planet.py
@@ -3,11 +3,11 @@ import random as r
 import drill as d
 from tabulate import tabulate
 
-
+# Here for out of scope loop breaking
 class ScopeBreak(Exception):
     pass
 
-
+# Main class in this module
 class planet:
     planetMap = []
     drills: dict[str, d.drill] = {}
@@ -17,7 +17,7 @@ class planet:
     def __init__(self, name):
         self.name = name
         self.planetMap = self.resourceScatter()
-        self.drills = self.initializeDrillVacancy()
+        self.drills = self.initializeDrillVacancy(13, 13)
         self.lowTier = r.choice(["Iron", "Coal", "Copper", "Aluminum"])
         self.midTier = r.choice(["Gold", "Zinc", "Lithium"])
         self.highTier = r.choice(["Diamond", "Lead", "Plutonium"])
@@ -27,6 +27,11 @@ class planet:
         return self.name
     
     def generateGrid(self, x, y, fill):
+        '''
+        This function generates a 2-dimensional array based on the height y
+        and the width x, fills the values of the array with whatever is passed
+        as fill
+        '''
         ret = []
         for i in range(0, y):
             ret.append([])
@@ -35,15 +40,30 @@ class planet:
         return ret
     
     def initializeUserMap(self, x, y):
+        '''
+        This function adds a coordinate system to the user map for
+        ease of use
+        '''
         map = self.generateGrid(x, y, '???')
+        # Loops through x and y independently, adding the scales
         for i in range(x):
             map[0][i] = i
         for i in range(y):
             map[i][0] = i
+        # Cleans out the (0,0) position, for cleanliness
         map[0][0] = ''
         return map
 
     def resourceScatter(self):
+        '''
+            This function will create a 2-dimensional array with
+        smooth value blending
+            It will add a randomized value between 0 and 1 on every 3rd point
+        It will then linearly interpolate (lerp) both horizontally and
+        vertically on every 3rd
+            And lastly it will average the horizontal and vertical lerp
+        for each 1st and 2nd in the x and y directions
+        '''
         #Generate base plane
         surface = self.generateGrid(13, 13, None)
 
@@ -61,7 +81,7 @@ class planet:
                     surface[i][3*j] = self.lerp(surface[3*int(segment)][3*j],
                                             surface[3*(int(segment)+1)][3*j],
                                             segment-int(segment))
-                except IndexError:
+                except IndexError: # Defaults to a 50% concentration at edges
                     surface[i][3*j] = self.lerp(surface[3*int(segment)][3*j],
                                             0.5,
                                             segment-int(segment))
@@ -74,7 +94,7 @@ class planet:
                     surface[3*i][j] = self.lerp(surface[3*i][3*int(segment)],
                                             surface[3*i][3*(int(segment)+1)],
                                             segment-int(segment))
-                except IndexError:
+                except IndexError: # Defaults to a 50% concentration at edges
                     surface[3*i][j] = self.lerp(surface[3*i][3*int(segment)],
                                             0.5,
                                             segment-int(segment))
@@ -93,16 +113,25 @@ class planet:
                     ))/2
         return surface
     
-    def initializeDrillVacancy(self):
+    def initializeDrillVacancy(self, x, y):
+        '''
+        This function generates a dictionary containing each tiles
+        vacancy for a drill
+        '''
         ret = {}
-        for x in range(13):
-            for y in range(13):
-                ret[f'{x},{y}'] = None
+        for i in range(x):
+            for j in range(y):
+                ret[f'{i},{j}'] = None
         return ret
 
     def addDrill(self):
+        '''
+        This function adds a drill to the current tile if vacant
+        '''
+        # Update user map
         self.userMap[self.y+1][self.x+1] += '\n(Drilling)'
         if self.drills[f'{self.x},{self.y}'] == None:
+            # Add the drill
             self.drills[f'{self.x},{self.y}'] = d.drill(
                                                 self.planetMap[self.x][self.y],
                                                 self.lowTier, 
@@ -114,8 +143,16 @@ class planet:
             print("There is already a drill there!")
 
     def groundSurvey(self):
+        '''
+        This function will add the ore concentration value to
+        the user map, and inform the player if it is a good
+        spot to mine or not
+        '''
+        # Convert ore concentration to a percentage
         tile = 100*self.planetMap[self.y][self.x]
+        # Update user map
         self.userMap[self.y+1][self.x+1] = f'{round(tile, 1)}%'
+        # Give user feedback
         if tile != None:
             if tile > 75:
                 print(f'{round(tile, 1)}% Ore Concentration, very good!')
@@ -127,11 +164,18 @@ class planet:
                 print(f'{round(tile, 1)}% Ore Concentration, rather awful.')
 
     def lerp(self, a, b, t):
+        '''
+        This function is a simple linear interpolation equation
+        '''
         return (1-t)*a + t*b
     
     def movePos(self):
+        '''
+        This function is used to allow the user to select a given tile
+        '''
         x_pos = -1
         y_pos = -1
+        # Get x from user
         while x_pos > 0 or x_pos <= 14:
             try:
                 x_pos = int(input("Please select an x position [1-13]: "))
@@ -141,6 +185,7 @@ class planet:
                 print("Please select a number from 1-13 (inclusive)")
             else:
                 break
+        # Get y from user
         while y_pos > 0 or y_pos <= 14:
             try:
                 y_pos = int(input("Please select a y position [1-13]: "))
@@ -154,24 +199,39 @@ class planet:
         self.y = y_pos - 1
 
     def leavePlanet(self):
+        '''
+        This function leaves the planet by breaking the planet options loop
+        in the main file, raising an error is required to do this so here we
+        utilize the ScopeBreak error defined at the top
+        '''
         self.x = self.y = 0
         print("Returning to station...")
         raise ScopeBreak
     
     def harvest(self, player):
+        '''
+        This function will collect all the minerals mined by the drills,
+        and display the total to the user
+        '''
         total = [0,0,0]
         for key in self.drills.keys():
             if self.drills[key] != None:
-                produced = self.drills[key].mine(player) # Display total accumulated resources
+                # Mine the resources
+                produced = self.drills[key].mine(player) 
+                # Track total production
                 total[0] += produced[0]
                 total[1] += produced[1]
                 total[2] += produced[2]
-        print('You produced:\n   '\
-             + f'{self.lowTier}: {total[0]}\n   '\
-             + f'{self.midTier}: {total[1]}\n   '\
+        # Display total accumulated resources
+        print('You produced:\n   ' \
+             + f'{self.lowTier}: {total[0]}\n   ' \
+             + f'{self.midTier}: {total[1]}\n   ' \
              + f'{self.highTier}: {total[2]}')
         
     def displayMap(self):
+        '''
+        This function will print the user map using the tabulate library
+        '''
         print(tabulate(self.userMap))
     
     planetOptions = { # Won't compile without this being at the bottom ¯\_(ツ)_/¯

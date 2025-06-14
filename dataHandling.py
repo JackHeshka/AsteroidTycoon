71a26305e22cc851a28985824204d771b69177a7
JackHeshka <jack.heshka@rbe.sk.ca>
2025-06-13 18:14:10 -0600

    Beta version of pickling completed

--- dataHandling.py
+++ dataHandling.py
@@ -3,27 +3,39 @@ This module is used for handling the saving and loading of player data
 from past and current sessions
 """
 import pickle as pickle
+import inspect as inspect
 import planet as planet
 import player as player
 import station as station
 
-def saveData(user:player.player):
+def saveData(user:player.player, station:station.spaceStation):
     planet_pkg = {}
-    player_pkg = {}
-    station_pkg = {}
 
     for n, p in user.planets:
         planet_pkg[n] = p
 
+    player_pkg = user
+
+    station_pkg = station
+
     with open(f"saves\{user.name}_planetData.pkl", 'wb') as file:
         pickle.dump(planet_pkg, file)
 
-    with open(f"saves\{user.name}_playerDat.pkl", 'wb') as file:
+    with open(f"saves\{user.name}_playerData.pkl", 'wb') as file:
         pickle.dump(player_pkg, file)
 
-    with open(f"saves\{user.name}_stationDat.pkl", 'wb') as file:
+    with open(f"saves\{user.name}_stationData.pkl", 'wb') as file:
         pickle.dump(station_pkg, file)
 
 
-def loadData():
-    pass
+def loadData(user:str):
+    with open(f"saves\{user}_planetData.pkl", 'rb') as file:
+        planet_pkg = pickle.load(file)
+
+    with open(f"saves\{user}_playerData.pkl", 'rb') as file:
+        player_pkg = pickle.load(file)
+
+    with open(f"saves\{user}_stationData.pkl", 'rb') as file:
+        station_pkg = pickle.load(file)
+
+    return [planet_pkg, player_pkg, station_pkg]
